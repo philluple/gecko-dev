6c9b89b9cced9e5a1b044a966a2716554759865c
philluple <pnl2111@columbia.edu>
2025-04-08 13:27:59 -0400

    Running simple integration test

--- third_party/rust/pdslib/src/lib.rs
+++ third_party/rust/pdslib/src/lib.rs
@@ -3,3 +3,18 @@ pub mod events;
 pub mod mechanisms;
 pub mod pds;
 pub mod queries;
+
+pub fn hello() {
+  println!("Hello from pdslib!");
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn test_hello() {
+      hello();
+      assert_eq!(2 + 2, 4);
+  }
+}
